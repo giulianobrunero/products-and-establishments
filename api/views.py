1e28a360a2159408cb9beb03b239476d23d5fcb9
giuliano brunero <giuliano.brunero@gmail.com>
2022-07-05 15:08:09 -0300

    Agrego vista para crear establecimiento, *Establishment*.

--- api/views.py
+++ api/views.py
@@ -6,13 +6,16 @@ from api.models import (Establishment, ProductBase, ProductEstablishment)
 from api.serializers import (EstablishmentSerializer, 
 	ProductBaseSerializer, ProductEstablishmentSerializer)
 
+
 class CreateProductBase(generics.CreateAPIView):
 	queryset = ProductBase.objects.all()
 	serializer_class = ProductBaseSerializer
 
+
 class ListProductsBase(generics.ListAPIView):
 	queryset = ProductBase.objects.all()
 	serializer_class = ProductBaseSerializer
+
 
 class UpdateProductBase(generics.RetrieveUpdateAPIView):
 	queryset = ProductBase.objects.all()
@@ -24,11 +27,14 @@ class DeleteProductBase(generics.DestroyAPIView):
     queryset = ProductBase.objects.all()
     serializer_class = ProductBaseSerializer
 '''
+#class ListProductsEstablishments(generics.ListAPIView)
+
 
 class CreateProductEstablishment(generics.ListCreateAPIView):
 	queryset = ProductEstablishment.objects.all()
 	serializer_class = ProductEstablishmentSerializer
 
 
-
-#class ListProductsEstablishments(generics.ListAPIView)
+class CreateEstablishment(generics.CreateAPIView):
+	queryset = Establishment.objects.all()
+	serializer_class = EstablishmentSerializer
