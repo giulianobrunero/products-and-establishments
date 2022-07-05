1094e8846b62ce1a8466c66707f929073342f464
giuliano brunero <giuliano.brunero@gmail.com>
2022-07-05 19:32:15 -0300

    agrego saltos de linea.

--- products_and_establishments/urls.py
+++ products_and_establishments/urls.py
@@ -5,6 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Products and establishments API",
@@ -17,6 +18,7 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=[permissions.AllowAny],
 )
+
 
 urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
