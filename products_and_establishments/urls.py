1b3e36cc55e7b010afd634a8a228916caeee8b8e
giuliano brunero <giuliano.brunero@gmail.com>
2022-07-05 19:19:25 -0300

    Agrego urls para swager y open api (docuimentacion).

--- products_and_establishments/urls.py
+++ products_and_establishments/urls.py
@@ -14,11 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from api import urls as api_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Products and establishments API",
+        default_version='v1',
+        description="",
+        terms_of_service="",
+        contact=openapi.Contact(email="giuliano.brunero@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
-	path('api-auth/', include('rest_framework.urls')),
-	path('api/', include(api_urls)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(api_urls))
 ]
+
