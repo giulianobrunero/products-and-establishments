48df16f4f28c5915b778a2aabc3f074ab69b4c29
giuliano brunero <giuliano.brunero@gmail.com>
2022-07-05 00:20:49 -0300

    Modifico los modelos *ProductBase* y *Product*, agregando el atributo *max_digits* a los campos decimales. Tambien arreglo un error de tipeo.

--- api/models.py
+++ api/models.py
@@ -17,9 +17,9 @@ class ProductBase(models.Model):
 	code = models.CharField(max_length=20, null=True, blank=True)
 	brand = models.CharField(max_length=100, null=False, blank=False)
 	product_type = models.CharField(max_length=100, null=False, blank=False)
-	calories = models.DecimalField(decimal_places=2, null=False, blank=False)
-	saturated_fat_percent = models.DecimalField(decimal_places=2, null=False, blank=False)
-	sugar_percent = models.DecimalField(decimal_places=2, null=False, blank=False)
+	calories = models.DecimalField(max_digits=3, decimal_places=2, null=False, blank=False)
+	saturated_fat_percent = models.DecimalField(max_digits=3, decimal_places=2, null=False, blank=False)
+	sugar_percent = models.DecimalField(max_digits=3, decimal_places=2, null=False, blank=False)
 	description = models.TextField(blank=True, null=True)
 
 	def __str__(self):
@@ -30,12 +30,12 @@ class Product(models.Model):
 	product_base = models.ForeignKey(ProductBase,
 		null=False,
 		blank=False,
-		verbose_name=_("ProductBase"),
+		verbose_name="ProductBase",
 		on_delete=models.CASCADE)
-	establishment = models.ForeignKey(ProductBase,
+	establishment = models.ForeignKey(Establishment,
 		null=False,
 		blank=False,
-		verbose_name=_("Establishment"),
+		verbose_name="Establishment",
 		on_delete=models.CASCADE)
-	price = models.DecimalField(decimal_places=2, null=False, blank=False)
+	price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False)
 	quantity = models.PositiveBigIntegerField(null=False, blank=False)
