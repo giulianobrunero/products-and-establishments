bd6d9c1649ce519614daf9633660439e124f7cf6
giuliano brunero <giuliano.brunero@gmail.com>
2022-07-05 14:40:38 -0300

    Modifico los serializers de la app *api*, no es necesario que especifique todos los campos uno por uno (excepto en casos donde necesite solo algunos y no todos).

--- api/serializers.py
+++ api/serializers.py
@@ -5,36 +5,16 @@ from api.models import (Establishment, ProductBase, Product)
 class EstablishmentSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Establishment
-		fields = [
-			'name',
-			'code',
-			'address',
-			'opening_hours',
-			'description'
-		]
+		fields = '__all__'
 
 
 class ProductBaseSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ProductBase
-		fields = [
-			'name',
-			'code',
-			'brand',
-			'product_type',
-			'calories',
-			'saturated_fat_percent',
-			'sugar_percent',
-			'description'
-		]
+		fields = '__all__'
 
 
 class ProductSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Product
-		fields = [
-			'product_base',
-			'establishment',
-			'price',
-			'quantity'
-		]
+		fields = '__all__'
