907cc218581917f6b7ec8cf7c0f0d08868d81d8b
giuliano brunero <giuliano.brunero@gmail.com>
2022-07-05 00:07:43 -0300

    creo una aplicacion n de django llamada **api**

---
+++ api/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Establishment(models.Model):
+	name = models.CharField(max_length=140, null=False, blank=False)
+	code = models.CharField(max_length=20, null=True, blank=True)
+	address = models.CharField(max_length=200, null=False, blank=False)
+	opening_hours = models.CharField(max_length=140, null=False, blank=False)
+	description = models.TextField(blank=True, null=True)
+
+	def __str__(self):
+		return f'{self.name} | {self.code}'
+
+
+class ProductBase(models.Model):
+	name = models.CharField(max_length=100, null=False, blank=False)
+	code = models.CharField(max_length=20, null=True, blank=True)
+	brand = models.CharField(max_length=100, null=False, blank=False)
+	product_type = models.CharField(max_length=100, null=False, blank=False)
+	calories = models.DecimalField(decimal_places=2, null=False, blank=False)
+	saturated_fat_percent = models.DecimalField(decimal_places=2, null=False, blank=False)
+	sugar_percent = models.DecimalField(decimal_places=2, null=False, blank=False)
+	description = models.TextField(blank=True, null=True)
+
+	def __str__(self):
+		return f'{self.name} | {self.code}'
+
+
+class Product(models.Model):
+	product_base = models.ForeignKey(ProductBase,
+		null=False,
+		blank=False,
+		verbose_name=_("ProductBase"),
+		on_delete=models.CASCADE)
+	establishment = models.ForeignKey(ProductBase,
+		null=False,
+		blank=False,
+		verbose_name=_("Establishment"),
+		on_delete=models.CASCADE)
+	price = models.DecimalField(decimal_places=2, null=False, blank=False)
+	quantity = models.PositiveBigIntegerField(null=False, blank=False)
