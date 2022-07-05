b6bed905f085a66d1b789c3723c78ecb6dc456f4
giuliano brunero <giuliano.brunero@gmail.com>
2022-07-05 16:37:05 -0300

    Agrego la vista *ListProductsEstablishment* que se encarga de mostrar los objetos solicitados segun una lista de filtros (calorias de un producto, precio, porcentaje de grasas, porcentaje de azucar, establecimiento, nombre del producto).

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework import permissions
 from api.models import (Establishment, ProductBase, ProductEstablishment)
 from api.serializers import (EstablishmentSerializer, 
 	ProductBaseSerializer, ProductEstablishmentSerializer)
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CreateProductBase(generics.CreateAPIView):
@@ -22,14 +23,6 @@ class UpdateProductBase(generics.RetrieveUpdateAPIView):
 	serializer_class = ProductBaseSerializer
 
 
-'''
-class DeleteProductBase(generics.DestroyAPIView):
-    queryset = ProductBase.objects.all()
-    serializer_class = ProductBaseSerializer
-'''
-#class ListProductsEstablishments(generics.ListAPIView)
-
-
 class CreateProductEstablishment(generics.CreateAPIView):
 	queryset = ProductEstablishment.objects.all()
 	serializer_class = ProductEstablishmentSerializer
@@ -43,3 +36,20 @@ class CreateEstablishment(generics.CreateAPIView):
 class ListEstablishments(generics.ListAPIView):
 	queryset = Establishment.objects.all()
 	serializer_class = EstablishmentSerializer
+
+
+class ListProductsEstablishment(generics.ListAPIView):
+	queryset = ProductEstablishment.objects.all()
+	serializer_class = ProductEstablishmentSerializer
+	filter_backends = [DjangoFilterBackend]
+	filterset_fields = {
+		# Product
+		'product_base__name': ['contains'],
+		'product_base__calories': ['gte', 'lte'],
+		'product_base__saturated_fat_percent': ['gte', 'lte'],
+		'product_base__sugar_percent': ['gte', 'lte'],
+		# Establishment
+		'establishment__name': ['contains'],
+		# Product in Establishment
+		'price': ['gte', 'lte']
+	}
