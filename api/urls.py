93651417b6e045d688e3582179c04016e223282d
giuliano brunero <giuliano.brunero@gmail.com>
2022-07-05 14:55:32 -0300

    Importo la vista *CreateProductEstablishment* en archivo de urls.

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from api.views import (CreateProductBase, ListProductsBase, UpdateProductBase)
+from api.views import (CreateProductBase, ListProductsBase, UpdateProductBase,
+	CreateProductEstablishment)
 
 urlpatterns = [
 	path('products/create', CreateProductBase.as_view(), name = 'create-product'),
 	path('products/list', ListProductsBase.as_view(), name = 'list-products'),
 	path('products/update/<int:pk>', UpdateProductBase.as_view(), name = 'update-product'),
 	#path('establishment/create', CreateEstablishment.as_view(), name = 'create-establishment'),
-	#path('products-establishment/create', CreateProductEstablishment.as_view(), name = 'create-product-establishment'),
+	path('products-establishment/create', CreateProductEstablishment.as_view(), name = 'create-product-establishment'),
 	#path('products-establishment/list', ListProductsEstablishment.as_view(), name = 'list-products-establishment'),
 ]
